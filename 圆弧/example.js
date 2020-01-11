c8b8da8a5407c3d11dccf37640f4b0cd01752f88
joanChen <joan.zhou2@gmail.com>
2020-01-11 19:26:51 +0800

    圆弧

--- 圆弧/example.js
+++ 圆弧/example.js
@@ -1,7 +1,36 @@
 var canvas = document.getElementById('canvas');
 var context = canvas.getContext('2d');
 
-context.beginPath();
+function drawGrid(context, color, stepx, stepy) {
+  context.save();
+  context.strokeStyle = color;
+  context.lineWidth = 0.5;
+  for (var i = stepx + 0.5; i < context.canvas.width; i += stepx) {
+    context.beginPath();
+    context.moveTo(i, 0);
+    context.lineTo(i, context.canvas.height);
+    context.stroke();
+  }
 
-// 起点
-context.moveTo();
+  for (var i = stepy + 0.5; i < context.canvas.height; i += stepy) {
+    context.beginPath();
+    context.moveTo(0, i);
+    context.lineTo(context.canvas.width, i);
+    context.stroke();
+  }
+  context.restore();
+}
+
+drawGrid(context, 'lightgray', 100, 100);
+
+// 第一条圆弧
+context.beginPath();
+context.moveTo(200, 100);
+context.arcTo(300, 100, 300, 300, 200);
+context.stroke();
+
+// 
+context.beginPath();
+context.moveTo(400, 100);
+context.arcTo(500, 100, 500, 300, 50);
+context.stroke();
